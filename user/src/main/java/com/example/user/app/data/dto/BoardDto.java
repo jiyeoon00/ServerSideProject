743f91818ec88b4265638ae8779bf6fb2fd3d227
jiyeon <sjy6736@naver.com>
2024-04-11 20:24:51 +0900

    feat: BoardDto 생성

---
+++ user/src/main/java/com/example/user/app/data/dto/BoardDto.java
@@ -0,0 +1,25 @@
+package com.example.user.app.data.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardDto {
+	private int boardId;
+	private String custId;
+	private String boardTitle;
+	private String boardContent;
+	private LocalDateTime boardRegdate;
+	private LocalDateTime boardUpdate;
+	private int boardCnt;
+	// 조회시 comment 개수 확인
+	private int commentCnt;
+	private List<CommentDto> commentList;
+}
