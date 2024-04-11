29b1dd23f00dcc416a0354546e9742052d3d0caa
jiyeon <sjy6736@naver.com>
2024-04-11 10:25:08 +0900

    feat: ItemRepository 구현

--- user/src/main/java/com/example/user/app/repository/ItemRepository.java
+++ user/src/main/java/com/example/user/app/repository/ItemRepository.java
@@ -1,11 +1,11 @@
 package com.example.user.app.repository;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
-import com.example.user.app.data.dto.ItemDto;
 import com.example.user.app.frame.HanaRepository;
+import com.example.user.app.data.dto.ItemDto;
 
 @Mapper
 public interface ItemRepository extends HanaRepository<Integer, ItemDto> {
 }
+
